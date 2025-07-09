f33533b4a7a1472588914a2322cef46cc8f7161e
KIRILL <batatik228@gmail.com>
2025-07-09 20:59:23 +0300

    lesson260625

---
+++ hw31.py
@@ -0,0 +1,15 @@
+import requests
+
+URL = ('https://dummyjson.com/carts')
+params = {'limit': 9999, 'skip': 0}
+response = requests.get(url=URL, params=params)
+carts = response.json()["carts"]
+
+response = requests.get(url=URL, params=params)
+
+total = 0
+for cart in carts:
+    if cart["userId"] <= 25:
+        total += cart["total"]
+
+print(f"Price: {total}")
